0fa19b9591fe99195732ce91dd8e24c40677481f
Manahil Ameer <manahil1054@gmail.com>
2019-11-06 17:19:14 +0500

    New cpp file

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ifstream in;
+	in.open("input.txt");
+	int list[20];
+	int i = 0;
+	if(in.fail())
+	cout<<"Your file is not working"<<endl;
+	else{
+	while (in>>list[i])
+	{
+		i++;
+	}
+	long long ans=0;
+	for (int j = 0; j < i; j++) {
+		ans = list[j] + (ans * 10);
+	}
+	cout << ans << endl;
+}
+}
